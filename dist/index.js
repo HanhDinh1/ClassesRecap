bafb7e294a2d67905fec909b015dda4f630472cb
HanhDinh1 <ms.myhanh89@gmail.com>
2023-10-21 01:20:14 -0400

    Class Keyword, constructors

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,15 @@
-"use strict";
-console.log("it's working!");
-console.log("yay!");
+class Player {
+constructor(first, last) {
+    this.first = first;
+    this.last = last;
+}
+    taunt(){
+        console.log("BOOYAH!")
+    }
+}   
+
+const Player1 = new Player("blue", "Steel");
+Player1.taunt();
+
+const Player2 = new Player("kenny", "Brow");
+console.log(Player1.first);
