ad4d328dced14fb35d43d6b0e7fff968d9aca8ad
HanhDinh1 <ms.myhanh89@gmail.com>
2023-10-22 01:41:00 -0400

    private field

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,16 @@
 class Player {
-    score = 0;
-    numLives = 10;
+    #score = 0; 
+    #numLives = 10;
 constructor(first, last) {
     this.first = first;
     this.last = last;
     
+}
+getScore() {
+    return this.#score;
+}
+updateScore(newScore) {
+    this.#score = newScore;
 }
     taunt(){
         console.log("BOOYAH!")
@@ -21,6 +27,12 @@ const player1 = new Player("blue", "Steel");
 // console.log(Player1.first);
 // console.log(Player1);\
 
-console.log(player1.numLives);
-player1.loseLife();
-console.log(player1.numLives);
+// console.log(player1.#numLives);
+// player1.loseLife();
+// console.log(player1.#numLives);
+
+console.log(player1.getScore());
+console.log(player1.updateScore(28));
+console.log(player1.getScore());
+
+console.log(player1);
