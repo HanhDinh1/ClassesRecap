40234e0239b0762f4388e36be18850ad1b4c5ab6
HanhDinh1 <ms.myhanh89@gmail.com>
2023-10-22 01:24:12 -0400

    class field

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,26 @@
 class Player {
+    score = 0;
+    numLives = 10;
 constructor(first, last) {
     this.first = first;
     this.last = last;
+    
 }
     taunt(){
         console.log("BOOYAH!")
     }
+    loseLife() {
+        this.numLives -= 1;
+    }
 }   
 
-const Player1 = new Player("blue", "Steel");
-Player1.taunt();
+const player1 = new Player("blue", "Steel");
+// Player1.taunt();
 
-const Player2 = new Player("kenny", "Brow");
-console.log(Player1.first);
+// const Player2 = new Player("kenny", "Brow");
+// console.log(Player1.first);
+// console.log(Player1);\
+
+console.log(player1.numLives);
+player1.loseLife();
+console.log(player1.numLives);
