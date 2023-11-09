2d3e4296d24c304a3a8e62e18c2f9ae190e732cb
HanhDinh1 <ms.myhanh89@gmail.com>
2023-11-08 23:03:19 -0500

    short cut syntax

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,24 @@
 "use strict";
+// class Player {
+//     public readonly first: string; 
+//     public readonly last: string;
+//     private score: number = 0;
+// constructor (first: string, last: string) {
+//     this.first = first;
+//     this.last = last;
+//     }
+//     private secretMethod(): void {
+//         console.log("SECRET METHOD");
+//     }
+// }
 class Player {
     constructor(first, last) {
-        this.score = 0;
         this.first = first;
         this.last = last;
+        this.score = 0;
+    } //short cut syntax
+    secretMethod() {
+        console.log("SECRET METHOD");
     }
 }
 const elton = new Player("Elton", "Steele");
-elton.score = 28;
